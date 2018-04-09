6e9e84c91447a742ece3f9dfbd79d99f3cbd9ca3
Jonathan <jonathan.lake1@live.com>
2018-04-09 17:03:12 +1200

    Add project files.

---
+++ ContentFilterPrototype_v1/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ContentFilterPrototype_v1
+{
+    [Activity(Label = "ContentFilterPrototype_v1", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Button signUp;
+        private Button submitNewUser;
+        private EditText txtUsername;
+        private EditText txtEmail;
+        private EditText txtPassword;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            signUp = FindViewById<Button>(Resource.Id.btnSignUp);
+            submitNewUser = FindViewById<Button>(Resource.Id.btnSave);
+            txtUsername = FindViewById<EditText>(Resource.Id.txtUsername);
+            txtEmail = FindViewById<EditText>(Resource.Id.txtEmail);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+
+            signUp.Click += (object sender, EventArgs args) =>
+            {
+                FragmentTransaction transFrag = FragmentManager.BeginTransaction();
+                OnSignUpEvent.SignUpDialog diagSignUp = new OnSignUpEvent.SignUpDialog();
+                diagSignUp.Show(transFrag, "Fragment Dialog");
+                diagSignUp.OnSignUpComplete += diagSignUp_onSignUpComplete;
+            };
+        }
+
+        private void diagSignUp_onSignUpComplete(object sender, OnSignUpEvent e)
+        {
+            StartActivity(typeof(Activity));
+        }
+        //protected override void OnCreate(Bundle savedInstanceState)
+        //{
+        //    base.OnCreate(savedInstanceState);
+
+        //    // Set our view from the "main" layout resource
+        //    SetContentView(Resource.Layout.Main);
+        //    Button button = FindViewById<Button>(Resource.Id.btnButton);
+        //    button.Click += delegate
+        //    {
+        //        button.Text = "Hello world I am your first App!";
+
+        //    };
+        //}
+    }
+}
+
---
+++ ContentFilterPrototype_v1/signUpDialog.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ContentFilterPrototype_v1
+{
+    public class OnSignUpEvent : EventArgs
+    {
+        private string txtUserName;
+        private string txtEmail;
+        private string txtPassword;
+
+        public string UserName
+        {
+            get { return txtUserName; }
+            set
+            {
+                txtUserName = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return txtEmail; }
+            set { txtEmail = value; }
+        }
+
+        public string Password
+        {
+            get { return txtPassword; }
+            set { txtPassword = value; }
+        }
+
+        public OnSignUpEvent(string username, string email, string password, string txtUserName, string txtEmail, string txtPassword) : base()
+        {
+            //this.txtUserName = txtUserName;
+            //this.txtEmail = txtEmail;
+            //this.txtPassword = txtPassword;
+            UserName = username;
+            Email = email;
+            Password = password;
+        }
+
+        public class SignUpDialog : DialogFragment
+        {
+            private EditText txtUsername;
+            private EditText txtEmail;
+            private EditText txtPassword;
+            private Button btnSaveSignUp;
+            public event EventHandler<OnSignUpEvent> OnSignUpComplete;
+            public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+            {
+                base.OnCreateView(inflater, container, savedInstanceState);
+                var view = inflater.Inflate(Resource.Layout.registerDialog, container, false);
+                txtUserame = view.FindViewById<EditText>(Resource.Id.txtUserName);
+                txtEmail = view.FindViewById<EditText>(Resource.Id.txtEmail);
+                txtPassword = view.FindViewById<EditText>(Resource.Id.txtPassword);
+                btnSaveSignUp = view.FindViewById<Button>(Resource.Id.btnSave);
+                btnSaveSignUp.Click += btnSaveSignUp_Click;
+                return view;
+            }
+            void btnSaveSignUp_Click(object sender, EventArgs e)
+            {
+                OnSignUpComplete.Invoke(this, new OnSignUpEvent(txtUsername.Text, txtEmail.Text, txtPassword.Text));
+                this.Dismiss();
+            }
+        }
+    }
+}
